b20d4dab73735a30180fcc9d5009f5ef6ef0e4a5
Marko Gichevski <marko.gicevski@iwconnect.com>
2026-02-26 23:20:31 +0100

    Fix the navigation add SPA to index.html

---
+++ assets/react-select.esm-CzAb_5ZD.js
@@ -0,0 +1,1 @@
+import{u as s,S as o,a as S}from"./Select-ef7c0426.esm-uEr7u5Nf.js";import{r as e}from"./index-BiS1Tgf-.js";var n=e.forwardRef(function(a,t){var r=s(a);return e.createElement(o,S({ref:t},r))}),l=n;export{l as S};
